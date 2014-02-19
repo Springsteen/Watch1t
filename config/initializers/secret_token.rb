48efa7d9bd01ede2b582db6f51eaefe63e51c10e
Springsteen <martin.iv96@gmail.com>
2014-02-19 12:31:18 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Watch1t::Application.config.secret_key_base = '331ccbbe68d3975b6d4b90e803abc0f643b469eb97d0a78977f8300ebd48213c377ac0d459ba88cc580c64c2350666094cd55de3129ca15b02c4b001b973c391'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Watch1t::Application.config.session_store :cookie_store, key: '_Watch1t_session'
