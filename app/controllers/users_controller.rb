8f8d8ac3a3e72b5ad66660bc522100101e8fa398
hrisiz <hrisi.zarkov@abv.bg>
2014-03-01 02:20:14 +0200

    Add User Edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-
+  before_action :set_user
+  @logged_user
+  require 'digest/md5'
   #bezpolezen e toq index metod no ina4e 6te polzvam index faila za predstawqne na ne6tata
   # GET /users
   # GET /users.json
@@ -31,58 +33,73 @@ class UsersController < ApplicationController
   
   # GET /users/user_info
   def user_info
-	@logged_user=User.where(id: session[:user_id]).take
   end
 
   # GET /users/register
   def register
-    @users = User.new
   end
 
   #bezpolezno
   # GET /users/edit
   def edit
-	@logged_user=User.where(id: session[:user_id]).take
   end
 
   # POST /users/create
   def create
-	if params[:user].blank? || params[:password].blank? || params[:email].blank? || params[:secret_answer].blank? || params[:secret_question].blank?
-		respond_to do |format|
-			format.html { redirect_to '/users/register_form', notice: 'Problem'}
-		end
-	elsif !User.where(user: params[:user]).take.nil?
-		respond_to do |format|
-			format.html { redirect_to '/users/register_form', notice: 'This user already exist'}
-		end
-	else
-		require 'digest/md5'
-		@user = User.new(:country => params[:country],:skype => params[:skype],:user => params[:user],:password => Digest::MD5.hexdigest(params[:password]),:block_code => 1,:email => params[:email],:email_check => 1,:secret_question => params[:secret_question],:secret_answer => params[:secret_answer])
-		respond_to do |format|
-		  if @user.save
-			format.html { redirect_to '/users/index', notice: 'You was successfully created.' }
-		  else
-			format.html { redirect_to '/users/register_form', notice: 'Problem' }
-		  end
-		end
-	end
+    respond_to do |format|
+      if params[:user].blank? || params[:password].blank? || params[:email].blank? || params[:secret_answer].blank? || params[:secret_question].blank?
+         format.html { redirect_to '/users/register', notice: 'Problem'}
+      elsif !User.where(user: params[:user]).take.nil?
+        format.html { redirect_to '/users/register', notice: 'This user already exist.'}
+      elsif session[:code] != params[:check_code]
+        format.html { redirect_to '/users/register', notice: 'Wrong Code.'}
+      else
+        @user = User.new(:country => params[:country],:skype => params[:skype],:user => params[:user],:password => Digest::MD5.hexdigest(params[:password]),:block_code => 1,:email => params[:email],:email_check => 1,:secret_question => params[:secret_question],:secret_answer => params[:secret_answer])
+        if @user.save
+          format.html { redirect_to '/users/index', notice: 'You was successfully created.' }
+          session.delete(:code)
+        else
+          format.html { redirect_to '/users/register', notice: 'System Problem' }
+          session.delete(:code)
+        end
+      end
+     end
   end
   #ima o6te rabota
   def update
     respond_to do |format|
-      #if User.find(session[:user_id]).update(user_params)
+      if @logged_user.password == params[:password]
+        password = @logged_user.password
+      else
+        password = Digest::MD5.hexdigest(params[:password])
+      end
+      if params[:user].blank? || params[:password].blank? || params[:email].blank? || params[:secret_answer].blank? || params[:secret_question].blank?
+         format.html { redirect_to '/users/edit', notice: 'Problem'}
+      elsif User.where(user: params[:user]).count >= 1 && @logged_user.user != params[:user]
+        format.html { redirect_to '/users/edit', notice: 'This user already exist.'}
+      elsif @logged_user.update(:user => params[:user],:password => password,:email => params[:email],:email_check => 1,:secret_question => params[:secret_question],:secret_answer => params[:secret_answer],:country => params[:country],:skype => params[:skype])
         format.html { redirect_to '/users/index', notice: 'You was successfully updated.' }
-      #else
-        #format.html { render action: 'edit' }
-      #end
+      else
+        format.html { redirect_to '/users/index', notice: 'System Problem.' }
+      end
     end
   end
   
   def destroy
-    User.find(session[:user_id]).destroy
+    @logged_user.destroy
     session[:user_id] = nil;
     respond_to do |format|
       format.html { redirect_to '/users/index', notice: "You was successfully deleted." }
     end
   end
+  
+  private
+    def set_user
+      if !session[:user_id].nil?
+       session[:user_id] = session[:user_id];
+       @logged_user=User.find(session[:user_id])
+      else
+        @logged_user = nil;
+      end
+    end
 end
