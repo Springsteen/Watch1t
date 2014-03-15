d427a757a9e0843b8c6484e6d0f1ce43ee3c51cb
Martin Ivanov <martin.iv96@gmail.com>
2014-03-15 17:11:34 +0200

    Update application.rb
    Adding explanation for some code

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,10 @@ Bundler.require(:default, Rails.env)
 
 module Watch1t
   class Application < Rails::Application
+    #load all files in the lib directory
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
