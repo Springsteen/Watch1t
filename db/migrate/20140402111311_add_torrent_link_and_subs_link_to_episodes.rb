0e846cc998fba29ac62e9de1ffb6d0f2d91b1174
Springsteen <martin.iv96@gmail.com>
2014-04-06 16:24:48 +0300

    Migration Fix

--- db/migrate/20140402111311_add_torrent_link_and_subs_link_to_episodes.rb
+++
@@ -1,6 +0,0 @@
-class AddTorrentLinkAndSubsLinkToEpisodes < ActiveRecord::Migration
-  def change
-  	add_column :episodes, :torrent_link, :string
-  	add_column :episodes, :subs_link, :string
-  end
-end
--- db/migrate/20140403131154_add_column_to_season.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToSeason < ActiveRecord::Migration
-  def change
-  	add_column :seasons, :season, :integer
-  end
-end
